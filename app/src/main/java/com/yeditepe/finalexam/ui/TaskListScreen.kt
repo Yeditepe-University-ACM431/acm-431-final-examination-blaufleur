e7f032551e5b3d3b0a92f61c3d1ef9666821bdbb
Mehmet Aslan <128591951+blaufleur@users.noreply.github.com>
2026-01-06 14:40:56 +0300

    Q2: implement todos

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -2,22 +2,40 @@ package com.yeditepe.finalexam.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.yeditepe.finalexam.model.Task
+import com.yeditepe.finalexam.ui.theme.MyApplicationTheme
 import com.yeditepe.finalexam.viewmodel.TaskViewModel
 
 @Composable
 fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
 
     // TODO 3: Read task list from ViewModel
+    val state = viewModel.state.collectAsState()
 
     Column {
         // TODO 4: Display task titles and completion status
         // Use a simple Column or LazyColumn
+        LazyColumn {
+            items(state.value) { t ->
+                Card (modifier = Modifier.padding(8.dp)) {
+                    Text(text = t.title)
+                    Text(text = "Completed: ${t.isCompleted}")
+                }
+            }
+        }
     }
 }
 
@@ -30,4 +48,12 @@ fun TaskRow(task: Task, navController: NavController) {
             // TODO 3: Navigate to detail screen with task title
         }
     )
+}
+
+@Preview
+@Composable
+fun TaskListScreenPreview() {
+    MyApplicationTheme {
+        TaskListScreen()
+    }
 }
--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -1,13 +1,35 @@
 package com.yeditepe.finalexam.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.yeditepe.finalexam.model.Task
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
+    private val _state = MutableStateFlow<List<Task>>(listOf(
+        Task(
+            id = 1,
+            title = "First Task",
+            isCompleted = false
+        ),
+        Task(
+            id = 2,
+            title = "Second Task",
+            isCompleted = false
+        )
+    ))
+
+    val state = _state.asStateFlow()
 
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        val task = _state.value.find { t ->
+            t.id == taskId
+        } ?: return
+
+        task.isCompleted == !task.isCompleted
     }
 }
